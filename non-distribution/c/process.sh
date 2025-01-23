eee27da1cfe3841cead24ba4c47a007f8276ffe0
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-01-23 16:27:21 -0500

    add script

--- non-distribution/c/process.sh
+++ non-distribution/c/process.sh
@@ -6,3 +6,10 @@
 # Convert each line to one word per line, **remove non-letter characters**, make lowercase, convert to ASCII; then remove stopwords (inside d/stopwords.txt)
 # Commands that will be useful: tr, iconv, grep
 
+cd "$(dirname "$0")/.." || exit 1
+
+tr -s "[:space:]" "\n" \
+    | tr -dcs "[:alpha:]\n" "\n" \
+    | tr "[:upper:]" "[:lower:]" \
+    | iconv -t "ASCII//TRANSLIT" \
+    | grep -vxFf "d/stopwords.txt"
