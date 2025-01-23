8c8dbc21d5f4a6da5e214be50b7a39a77ea23baa
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-01-23 15:36:04 -0500

    implement get text

--- non-distribution/c/getText.js
+++ non-distribution/c/getText.js
@@ -11,13 +11,16 @@ const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
 });
+let content = "";
 
 rl.on('line', (line) => {
   // 1. Read HTML input from standard input, line by line using the `readline` module.
+  content += line + "\n";
 });
 
 // 2. after all input is received, use convert to output plain text.
 rl.on('close', () => {
+  console.log(convert(content));
 });
 
 
