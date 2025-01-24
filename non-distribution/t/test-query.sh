6dec97b0fdd226b5168ec1c5f1fa908bf241c005
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-01-24 13:08:12 -0500

    add query test

--- non-distribution/t/test-query.sh
+++ non-distribution/t/test-query.sh
@@ -15,8 +15,10 @@ cat "$T_FOLDER"/d/d7.txt > d/global-index.txt
 if $DIFF <(./query.js "$term") <(cat "$T_FOLDER"/d/d8.txt) >&2;
 then
     echo "$0 success: search results are identical"
+    cat /dev/null > d/global-index.txt
     exit 0
 else
     echo "$0 failure: search results are not identical"
+    cat /dev/null > d/global-index.txt
     exit 1
 fi
