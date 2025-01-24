754a525e97a69fffe31e8e43fec83e961249d63a
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-01-23 20:03:12 -0500

    add merge test

--- non-distribution/t/ts/s_test_merge.sh
+++ non-distribution/t/ts/s_test_merge.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 # This is a student test
 
+T_FOLDER=${T_FOLDER:-t}
+R_FOLDER=${R_FOLDER:-}
+
+cd "$(dirname "$0")/../..$R_FOLDER" || exit 1
+
+DIFF=${DIFF:-diff}
+DIFF_PERCENT=${DIFF_PERCENT:-0}
+
+
+cat "$T_FOLDER"/d/s_merge_global.txt > d/global-index.txt
+c/merge.js d/global-index.txt <"$T_FOLDER"/d/s_merge_local.txt > d/temp-global-index.txt
+mv d/temp-global-index.txt d/global-index.txt
+
+if DIFF_PERCENT=$DIFF_PERCENT t/gi-diff.js <(sort d/global-index.txt) <(sort "$T_FOLDER"/d/s_merge_result.txt) >&2;
+then
+    echo "$0 success: global indexes are identical"
+    cat /dev/null > d/global-index.txt
+    exit 0
+else
+    echo "$0 failure: global indexes are not identical"
+    cat /dev/null > d/global-index.txt
+    exit 1
+fi
