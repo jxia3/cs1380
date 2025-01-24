3ab314366e3d7f77dfc910ece05dfa5ee165aee3
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-01-24 17:41:04 -0500

    implement combine js

--- non-distribution/c/combine.sh
+++ non-distribution/c/combine.sh
@@ -3,6 +3,10 @@
 # Combine terms to create  n-grams (for n=1,2,3) and then count and sort them
 # Usage: ./combine.sh <terms > n-grams
 
+cd "$(dirname "$0")" || exit 1
+./combine.js
+
+: '
 p1=$(mktemp -u p1.XXXXXX)
 p2=$(mktemp -u p2.XXXXXX)
 p3=$(mktemp -u p3.XXXXXX)
@@ -12,3 +16,4 @@ mkfifo "$p1" "$p2" "$p3"
 tee >(sort) >(tee "$p1" | tail +2 | paste "$p1" - | sort) >(tee "$p2" | tail +2 | paste "$p2" - | tee "$p3" | cut -f 1 | tail +3 | paste "$p3" - | sort) > /dev/null
 
 rm "$p1" "$p2" "$p3"
+'
