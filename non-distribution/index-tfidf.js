a8c1c98bdf46420e7100a8e236a0bc2b9b9d5df5
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-01-26 20:19:47 -0500

    add tfidf index file

--- non-distribution/index-tfidf.js
+++ non-distribution/index-tfidf.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 
+/* An implementation of a TF-IDF indexer. Term frequency and document frequency metrics
+   are tracked in an auxiliary global index file. */
+
+const TFIDF_INDEX = 'd/tfidf-index.txt';
+const GLOBAL_INDEX = 'd/global-index.txt';
+
 const fs = require('fs');
 const natural = require('natural');
 const path = require('path');
@@ -12,4 +18,4 @@ if (process.argv.length < 4) {
 const contentFile = process.argv[2];
 const pageURL = process.argv[3];
 
-console.log(contentFile, pageURL)
+console.log(contentFile, pageURL);
