c0eb4fcc2e815c14b3572393bcdca4b31c113e2f
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-01-27 12:52:54 -0500

    add documents

--- non-distribution/t/ts/s_test_index_tfidf.js
+++ non-distribution/t/ts/s_test_index_tfidf.js
@@ -7,6 +7,17 @@
    - The fourth document contains 50 unique words.
    The TF-IDF scores for each term and document can then be computed and validated. */
 
+const fs = require('fs');
+const path = require('path');
+const childProcess = require('child_process');
+
+const DOCUMENT_FILES = [
+  path.join(__dirname, "../d/s_tfidf_1.txt"),
+  path.join(__dirname, "../d/s_tfidf_2.txt"),
+  path.join(__dirname, "../d/s_tfidf_3.txt"),
+  path.join(__dirname, "../d/s_tfidf_4.txt"),
+];
+
 // Generate words for the documents
 const SINGLE_REPEAT = 'lorem';
 const DOUBLE_REPEAT = ['ipsum', 'dolor'];
@@ -17,7 +28,17 @@ for (let w = 0; w < 50; w += 1) {
   const startCode = 'a'.charCodeAt(0);
   UNIQUE_WORDS.push(String.fromCharCode(startCode + first, startCode + second));
 }
-console.log(UNIQUE_WORDS);
+
+// Run TF-IDF indexer
+generateDocuments();
+
+/* Generates and saves synthetic documents. */
+function generateDocuments() {
+  fs.writeFileSync(DOCUMENT_FILES[0], generateSingleRepeat());
+  fs.writeFileSync(DOCUMENT_FILES[1], generateDoubleRepeat());
+  fs.writeFileSync(DOCUMENT_FILES[2], generateDoubleRepeat());
+  fs.writeFileSync(DOCUMENT_FILES[3], generateUnique());
+}
 
 /* Generates a document with a single word repeated 50 times. */
 function generateSingleRepeat() {
