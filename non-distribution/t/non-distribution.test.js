7e48df0e098d25931aaa9a5b411935e834690dc9
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-01-26 19:17:47 -0500

    uncomment

--- non-distribution/t/non-distribution.test.js
+++ non-distribution/t/non-distribution.test.js
@@ -73,7 +73,6 @@ test('(10 pts) test-end_to_end', () => {
   execSync(command, {stdio: ['pipe', 'pipe', 'inherit']}).toString().trim();
 });
 
-/* This test is broken
 test('(20 pts) test-sandbox_1', () => {
   const command = `"${path.join(__dirname, 'grade-sandbox-1.sh')}"`;
   execSync(command, {stdio: ['pipe', 'pipe', 'inherit']}).toString().trim();
