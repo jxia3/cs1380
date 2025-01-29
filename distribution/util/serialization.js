b70a6b7048b4de4167dff2a776b11dcdbe1f4122
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-01-28 19:37:28 -0500

    add structure

--- distribution/util/serialization.js
+++ distribution/util/serialization.js
@@ -21,21 +21,66 @@ const LeafTag = {
   Boolean: 'b',
   String: 's',
   Date: 'd',
-  Error: 'e',
 };
 
 // Non-leaf type flags
 const ObjectType = {
   Function: 'function',
+  Error: 'error',
   Array: 'array',
   Object: 'object',
 };
 
-/* Serializes a JavaScript object as a string. */
+/* Serializes a JavaScript object as a string. Cyclical references to functions, errors,
+   arrays, and objects are supported. Other reference types are not explicitly supported. */
 function serialize(object) {
+  const path = [];
+  const seen = new Map();
+  const result = encode(object, path, seen);
+  return JSON.stringify(result);
 }
 
-/* Deserializes a string into a JavaScript object. */
+/* Converts an object to a serializable JSON representation. */
+function encode(object, path, seen) {
+  // Encode leaf types
+  if (object === undefined) {
+    return encodeUndefined();
+  } else if (object === null) {
+    return encodeNull();
+  } else if (typeof object === 'number') {
+    return encodeNumber(object);
+  } else if (typeof object === 'boolean') {
+    return encodeBoolean(object);
+  } else if (typeof object === 'string') {
+    return encodeString(object);
+  } else if (object instanceof Date) {
+    return encodeDate(object);
+  }
+
+  // Encode reference types
+  if (object instanceof Function) {
+    return encodeFunction(object, path, seen);
+  } else if (object instanceof Error) {
+    return encodeError(object, path, seen);
+  } else if (object instanceof Array) {
+    return encodeArray(object, path, seen);
+  } else {
+    return encodeObject(object, path, seen);
+  }
+}
+
+/* Encodes an undefined object as its leaf tag. */
+function encodeUndefined() {
+  return LeafTag.Undefined;
+}
+
+/* Encodes a null object as its leaf tag. */
+function encodeNull() {
+  return LeafTag.Null;
+}
+
+/* Deserializes a string into a JavaScript object. The resulting object may contain
+   cyclical references in child functions, errors, arrays, and objects. */
 function deserialize(string) {
 }
 
