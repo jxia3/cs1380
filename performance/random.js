134fc3e3781729691d4a7c840f804fa7dc3a2c4b
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-01-30 16:15:04 -0500

    add primitive generate

--- performance/random.js
+++ performance/random.js
@@ -6,11 +6,13 @@ const MOD = 2147483647;
 let state = Math.floor(Math.random() * 1_000_000);
 
 function next() {
+  /* Returns a random number between 0 and 1 and advances the PRNG. */
   state = (state * FACTOR) % MOD;
   return state / (MOD - 1);
 }
 
 function chance(threshold) {
+  /* Returns true with a random chance and advances the PRNG. */
   const entropy = next();
   return entropy < threshold;
 }
