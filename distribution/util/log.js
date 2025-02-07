72b4abb80d00f585ae47569eb81a86123cc06fbd
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-02-07 15:18:46 -0500

    improve log

--- distribution/util/log.js
+++ distribution/util/log.js
@@ -1,28 +1,36 @@
-/* Create log file */
 const fs = require("fs");
 const path = require("path");
 
+/* Logs messages to the console and a log file. */
+
 const PRINT_MESSAGES = true;
 const SAVE_MESSAGES = false;
+const LOG_FILE = "log.txt";
 
-const logFile = path.join(__dirname, "../../", "log.txt");
+const logFile = path.join(__dirname, "../../", LOG_FILE);
 
+/* Appends timestamp and node information to a message and logs the message. */
 function log(message, severity) {
   if (severity === undefined) {
     severity = "info";
   }
-
   const now = new Date();
-  const date = `${new Intl.DateTimeFormat("en-GB",
-      {day: "2-digit", month: "2-digit", year: "numeric", hour: "2-digit",
-        minute: "2-digit", second: "2-digit", hour12: false})
-      .format(now)}.${String(now.getMilliseconds() * 1000).padStart(6, "0")}`;
+  const date = new Intl.DateTimeFormat("en-GB", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  }).format(now);
+  const timestamp = `${date}.${(now.getMilliseconds() * 1000).toString().padStart(6, "0")}`;
 
-  global.distribution.local.status.get("sid", (e, sid) => {
+  global.distribution.local.status.get("sid", (error, sid) => {
     if (sid === undefined) {
       sid = "unknown";
     }
-    const data = `${date} [${global.nodeConfig.ip}:${global.nodeConfig.port} (${sid})] [${severity}] ${message}`;
+    const data = `[${timestamp}] [${global.nodeConfig.ip}:${global.nodeConfig.port} (${sid})] [${severity}] ${message}`;
     if (PRINT_MESSAGES) {
       console.log(data);
     }
