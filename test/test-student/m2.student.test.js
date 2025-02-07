6c9d1e636b754384fa2286b22422276984dab6b7
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-02-07 16:24:11 -0500

    add complex rpc test

--- test/test-student/m2.student.test.js
+++ test/test-student/m2.student.test.js
@@ -7,17 +7,29 @@
 */
 
 const distribution = require("../../config.js");
+const id = distribution.util.id;
 const local = distribution.local;
+const node = distribution.node;
 const util = distribution.util;
 
 test("(1 pts) student test", (done) => {
-  // Fill out this test case...
-  done(new Error("Not implemented"));
-});
-
-test("(1 pts) student test", (done) => {
-  // Fill out this test case...
-  done(new Error("Not implemented"));
+  const remote = {node: node.config, service: "status", method: "get"};
+  local.status.get("counts", (error, startCount) => {
+    expect(error).toBeFalsy();
+    local.comm.send(["nid"], remote, (error, nid) => {
+      expect(error).toBeFalsy();
+      expect(nid).toBe(id.getNID(node.config));
+      local.comm.send(["ip"], remote, (error, ip) => {
+        expect(error).toBeFalsy();
+        expect(ip).toBe(node.config.ip);
+        local.status.get("counts", (error, endCount) => {
+          expect(error).toBeFalsy();
+          expect(endCount).toBe(startCount + 2);
+          done();
+        });
+      });
+    });
+  });
 });
 
 test("(1 pts) student test", (done) => {
@@ -36,9 +48,9 @@ test("(1 pts) student test", (done) => {
     value += amount;
     callback(null, value);
   }
+
   const stub = util.wire.createRPC(increment);
   expect(typeof stub).toBe("function");
-
   stub(1, (error, value) => {
     expect(error).toBeFalsy();
     expect(value).toBe(1);
@@ -47,6 +59,33 @@ test("(1 pts) student test", (done) => {
       expect(value).toBe(3);
       done();
     });
+  });
+});
+
+test("(1 pts) student test", (done) => {
+  const remote = {node: node.config, service: "status", method: "get"};
+  function getNodeInfo(callback) {
+    local.comm.send(["nid"], remote, (error, nid) => {
+      if (error) {
+        callback(error, null);
+        return;
+      }
+      local.comm.send(["sid"], remote, (error, sid) => {
+        if (error) {
+          callback(error, null);
+          return;
+        }
+        callback(null, [nid, sid]);
+      });
+    });
+  }
+
+  const stub = util.wire.createRPC(getNodeInfo);
+  expect(typeof stub).toBe("function");
+  stub((error, result) => {
+    expect(error).toBeFalsy();
+    expect(result).toEqual([id.getNID(node.config), id.getSID(node.config)]);
+    done();
   });
 });
 
