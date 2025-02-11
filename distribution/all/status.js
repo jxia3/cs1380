64cf173eaab309687c943ac3490b5bbd28fcbb80
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-02-11 13:39:42 -0500

    add file

--- distribution/all/status.js
+++ distribution/all/status.js
@@ -1,18 +1,33 @@
+/* Queries or modifies the state of all the nodes in a group. */
 
-const status = function(config) {
+/**
+ * Retrieves a status value for each node in the current group.
+ */
+function get(item, callback) {
+
+}
+
+/**
+ * Creates a new node in a child process with a configuration. The node information is
+ * sent to all the nodes in the current group.
+ */
+function spawn(config, callback) {
+
+}
+
+/**
+ * Stops all the nodes in the current group including the current node.
+ */
+function stop(callback) {
+
+}
+
+module.exports = (config) => {
   const context = {};
-  context.gid = config.gid || "all";
-
+  context.gid = config?.gid === undefined ? "all" : config.gid;
   return {
-    get: (config, callback) => {
-    },
-
-    spawn: (config, callback) => {
-    },
-
-    stop: (callback) => {
-    },
+    get: get.bind(context),
+    spawn: spawn.bind(context),
+    stop: stop.bind(context),
   };
 };
-
-module.exports = status;
