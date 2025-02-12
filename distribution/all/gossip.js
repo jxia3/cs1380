69c8912a47401847a2ac0516a9de5eff65f31daa
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-02-11 19:31:10 -0500

    add check

--- distribution/all/gossip.js
+++ distribution/all/gossip.js
@@ -6,21 +6,29 @@ const remote = require("./remote.js");
  * Sends a message to random nodes in the current group.
  */
 function send(message, config, callback) {
-  remote.checkGroup(this.gid);
+  checkContext(this.gid, this.subset);
 }
 
 /**
  * Schedules a function to be run in a periodic interval.
  */
 function at(interval, fn, callback) {
-
+  checkContext(this.gid, this.subset);
 }
 
 /**
  * Deletes a scheduled function.
  */
 function del(fnId, callback) {
+  checkContext(this.gid, this.subset);
+}
 
+/* Checks if the current function context is valid. */
+function checkContext(gid, subset) {
+  remote.checkGroup(gid);
+  if (typeof subset !== "function") {
+    throw new Error("Invalid gossip subset size function");
+  }
 }
 
 module.exports = (config) => {
