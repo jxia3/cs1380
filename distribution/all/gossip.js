5b1e7ccf89b5bb0737333e7ef66aeb19591609eb
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-02-11 19:29:11 -0500

    add gossip framework

--- distribution/all/gossip.js
+++ distribution/all/gossip.js
@@ -1,21 +1,38 @@
+/* Sends messages to all nodes in the current group probabilistically. */
 
-const gossip = function(config) {
+const remote = require("./remote.js");
+
+/**
+ * Sends a message to random nodes in the current group.
+ */
+function send(message, config, callback) {
+  remote.checkGroup(this.gid);
+}
+
+/**
+ * Schedules a function to be run in a periodic interval.
+ */
+function at(interval, fn, callback) {
+
+}
+
+/**
+ * Deletes a scheduled function.
+ */
+function del(fnId, callback) {
+
+}
+
+module.exports = (config) => {
   const context = {};
-  context.gid = config.gid || "all";
-  context.subset = config.subset || function(lst) {
-    return Math.ceil(Math.log(lst.length));
-  };
-
+  context.gid = config?.gid === undefined ? "all" : config.gid;
+  context.subset = config?.subset;
+  if (typeof context.subset !== "function") {
+    context.subset = (nodes) => Math.ceil(Math.log(nodes.length));
+  }
   return {
-    send: (payload, remote, callback) => {
-    },
-
-    at: (period, func, callback) => {
-    },
-
-    del: (intervalID, callback) => {
-    },
+    send: send.bind(context),
+    at: at.bind(context),
+    del: del.bind(context),
   };
 };
-
-module.exports = gossip;
