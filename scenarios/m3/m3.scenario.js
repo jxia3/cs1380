27096ae1895f467da0f6f1b189ac5e372cc31d29
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-02-13 17:36:17 -0500

    add scenario

--- scenarios/m3/m3.scenario.js
+++ scenarios/m3/m3.scenario.js
@@ -64,28 +64,30 @@ test("(5 pts) (scenario) group relativity", (done) => {
   const groupC = {};
 
   // Create groupC in an appropriate way...
-
+  groupC[id.getSID(n1)] = n1;
+  groupC[id.getSID(n2)] = n2;
 
   const config = {gid: "groupC"};
 
   distribution.local.groups.put(config, groupC, (e, v) => {
     distribution.groupC.groups.put(config, groupC, (e, v) => {
       // Modify the local 'view' of the group...
-
-      distribution.groupC.groups.get("groupC", (e, v) => {
-        const n1View = v[id.getSID(n1)];
-        const n2View = v[id.getSID(n2)];
-        try {
-          expect(Object.keys(n2View)).toEqual(expect.arrayContaining(
-              [id.getSID(n1), id.getSID(n2)],
-          ));
-          expect(Object.keys(n1View)).toEqual(expect.arrayContaining(
-              [id.getSID(n2)],
-          ));
-          done();
-        } catch (error) {
-          done(error);
-        }
+      distribution.local.groups.rem("groupC", id.getSID(n1), (e, v) => {
+        distribution.groupC.groups.get("groupC", (e, v) => {
+          const n1View = v[id.getSID(n1)];
+          const n2View = v[id.getSID(n2)];
+          try {
+            expect(Object.keys(n2View)).toEqual(expect.arrayContaining(
+                [id.getSID(n1), id.getSID(n2)],
+            ));
+            expect(Object.keys(n1View)).toEqual(expect.arrayContaining(
+                [id.getSID(n2)],
+            ));
+            done();
+          } catch (error) {
+            done(error);
+          }
+        });
       });
     });
   });
