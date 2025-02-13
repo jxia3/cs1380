8bacc46d7e9c2eddb0cab26b5300ea345732de21
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-02-13 16:12:25 -0500

    remove a

--- a.js
+++
@@ -1,7 +0,0 @@
-function(_0x5d6a20, _0x5979cf, _0x746bcf) {
-  _0x5979cf = _0x5979cf || {}, typeof _0x5d6a20 === 'string' && (_0x5d6a20 = {
-      'gid': _0x5d6a20
-  }), groupsStore[_0x5d6a20['gid']] = _0x5979cf, Object['keys'](_0x5979cf)['forEach'](_0x296273 => {
-      groupsStore['all'][id['getSID'](_0x5979cf[_0x296273])] = _0x5979cf[_0x296273];
-  }), global['distributi' + 'on'][_0x5d6a20['gid']] = {}, global['distributi' + 'on'][_0x5d6a20['gid']]['status'] = require('../all/sta' + 'tus')(_0x5d6a20), global['distributi' + 'on'][_0x5d6a20['gid']]['comm'] = require('../all/com' + 'm')(_0x5d6a20), global['distributi' + 'on'][_0x5d6a20['gid']]['gossip'] = require('../all/gos' + 'sip')(_0x5d6a20), global['distributi' + 'on'][_0x5d6a20['gid']]['groups'] = require('../all/gro' + 'ups')(_0x5d6a20), global['distributi' + 'on'][_0x5d6a20['gid']]['routes'] = require('../all/rou' + 'tes')(_0x5d6a20), global['distributi' + 'on'][_0x5d6a20['gid']]['mem'] = require('../all/mem')(_0x5d6a20), global['distributi' + 'on'][_0x5d6a20['gid']]['store'] = require('../all/sto' + 're')(_0x5d6a20), global['distributi' + 'on'][_0x5d6a20['gid']]['mr'] = require('../all/mr')(_0x5d6a20), _0x746bcf(null, _0x5979cf);
-}
