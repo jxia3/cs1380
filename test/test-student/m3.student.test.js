fa6de424b666ce1dedc0dfcba7bf784ff09d0945
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-02-14 12:00:52 -0500

    add test nodes

--- test/test-student/m3.student.test.js
+++ test/test-student/m3.student.test.js
@@ -8,10 +8,13 @@
 
 const distribution = require("../../config.js");
 
-test("(1 pts) student test", (done) => {
-  // Fill out this test case...
-  done(new Error("Not implemented"));
-});
+let localServer = null;
+const nodes = [
+  {ip: "127.0.0.1", port: 2000},
+  {ip: "127.0.0.1", port: 2001},
+  {ip: "127.0.0.1", port: 2002},
+  {ip: "127.0.0.1", port: 2003},
+];
 
 test("(1 pts) student test", (done) => {
   // Fill out this test case...
@@ -32,3 +35,49 @@ test("(1 pts) student test", (done) => {
   // Fill out this test case...
   done(new Error("Not implemented"));
 });
+
+test("(1 pts) student test", (done) => {
+  // Fill out this test case...
+  done(new Error("Not implemented"));
+});
+
+beforeAll((done) => {
+  stopNodes(() => {
+    distribution.node.start((server) => {
+      localServer = server;
+      distribution.local.status.spawn(nodes[0], (error, result) => {
+        distribution.local.status.spawn(nodes[1], (error, result) => {
+          distribution.local.status.spawn(nodes[2], (error, result) => {
+            distribution.local.status.spawn(nodes[3], (error, result) => {
+              done();
+            });
+          });
+        });
+      });
+    });
+  });
+});
+
+afterAll((done) => {
+  stopNodes(() => {
+    localServer.close();
+    done();
+  });
+});
+
+function stopNodes(callback) {
+  const stopMethod = {service: "status", method: "stop"};
+  stopMethod.node = nodes[0];
+  distirbution.local.comm.send([], stopMethod, (error, result) => {
+    stopMethod.node = nodes[1];
+    distribution.local.comm.send([], stopMethod, (error, result) => {
+      stopMethod.node = nodes[2];
+      distribution.local.comm.send([], stopMethod, (error, result) => {
+        stopMethod.node = nodes[3];
+        distribution.local.comm.send([], stopMethod, (error, result) => {
+          callback();
+        });
+      });
+    });
+  });
+}
