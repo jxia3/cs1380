1b9c357578e513fa1c1695ac2b3e4fb56d5615d7
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-02-20 19:53:33 -0500

    add check context

--- distribution/all/mem.js
+++ distribution/all/mem.js
@@ -1,12 +1,13 @@
 /* A service that stores key-value pairs in memory distributed across a group. */
 
+const remote = require("./remote.js");
 const util = require("../util/util.js");
 
 /**
  * Retrieves an item from group in-memory store using its key.
  */
 function get(config, callback) {
-
+  checkContext(this.gid, this.hash);
 }
 
 /**
@@ -14,21 +15,29 @@ function get(config, callback) {
  * SHA256 hash of the object serialized as JSON is used.
  */
 function put(config, callback) {
-
+  checkContext(this.gid, this.hash);
 }
 
 /**
  * Removes an item from the group in-memory store using its key.
  */
 function del(config, callback) {
-
+  checkContext(this.gid, this.hash);
 }
 
 /**
  * Rebalances items across a group when membership is changed.
  */
 function reconf(config, callback) {
+  checkContext(this.gid, this.hash);
+}
 
+/* Checks if the current function context is valid. */
+function checkContext(gid, hash) {
+  remote.checkGroup(gid);
+  if (typeof hash !== "function") {
+    throw new Error("Invalid store hash function");
+  }
 }
 
 module.exports = (config) => {
