1294559b6bb7842419c6721893b9d9701be754e8
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-02-20 19:51:52 -0500

    add

--- distribution/all/mem.js
+++ distribution/all/mem.js
@@ -1,24 +1,48 @@
+/* A service that stores key-value pairs in memory distributed across a group. */
 
-function mem(config) {
+const util = require("../util/util.js");
+
+/**
+ * Retrieves an item from group in-memory store using its key.
+ */
+function get(config, callback) {
+
+}
+
+/**
+ * Inserts an item into the group in-memory store. If a key is not specified, then the
+ * SHA256 hash of the object serialized as JSON is used.
+ */
+function put(config, callback) {
+
+}
+
+/**
+ * Removes an item from the group in-memory store using its key.
+ */
+function del(config, callback) {
+
+}
+
+/**
+ * Rebalances items across a group when membership is changed.
+ */
+function reconf(config, callback) {
+
+}
+
+module.exports = (config) => {
   const context = {};
-  context.gid = config.gid || "all";
-  context.hash = config.hash || global.distribution.util.id.naiveHash;
+  context.gid = config?.gid === undefined ? "all" : config.gid;
+  context.hash = config?.hash;
+  if (typeof context.hash !== "function") {
+    context.hash = util.id.naiveHash;
+  }
 
-  /* For the distributed mem service, the configuration will
-          always be a string */
   return {
-    get: (config, callback) => {
-    },
-
-    put: (state, config, callback) => {
-    },
-
-    del: (config, callback) => {
-    },
-
-    reconf: (config, callback) => {
-    },
+    get: get.bind(context),
+    put: put.bind(context),
+    del: del.bind(context),
+    reconf: reconf.bind(context),
   };
 };
-
-module.exports = mem;
