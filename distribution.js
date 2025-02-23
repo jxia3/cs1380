be4bdbc780f448c8843672e8841ac4ce5c69aa99
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-02-23 12:36:50 -0500

    reformat

--- distribution.js
+++ distribution.js
@@ -8,20 +8,20 @@ const args = require("yargs").argv;
 global.nodeConfig = global.nodeConfig || {
   ip: "127.0.0.1",
   port: 1234,
+  heartbeat: false,
   onStart: () => {},
 };
 
 /*
-You can pass "ip" and "port" arguments directly.
-Use this to startup nodes from the terminal.
+  You can pass "ip" and "port" arguments directly.
+  Use this to startup nodes from the terminal.
 
-Usage:
-./distribution.js --ip '127.0.0.1' --port 1234 # Start node on localhost:1234
-  */
+  Usage:
+  ./distribution.js --ip '127.0.0.1' --port 1234 # Start node on localhost:1234
+*/
 if (args.ip) {
   global.nodeConfig.ip = args.ip;
 }
-
 if (args.port) {
   global.nodeConfig.port = parseInt(args.port);
 }
@@ -29,10 +29,9 @@ if (args.port) {
 if (args.config) {
   const nodeConfig = util.deserialize(args.config);
   global.nodeConfig.ip = nodeConfig.ip ? nodeConfig.ip : global.nodeConfig.ip;
-  global.nodeConfig.port = nodeConfig.port
-        ? nodeConfig.port : global.nodeConfig.port;
-  global.nodeConfig.onStart = nodeConfig.onStart
-        ? nodeConfig.onStart : global.nodeConfig.onStart;
+  global.nodeConfig.port = nodeConfig.port ? nodeConfig.port : global.nodeConfig.port;
+  global.nodeConfig.heartbeat = nodeConfig.heartbeat ? true : false;
+  global.nodeConfig.onStart = nodeConfig.onStart ? nodeConfig.onStart : global.nodeConfig.onStart;
   if (nodeConfig._disableLogs) {
     try {
       log.disable();
@@ -47,7 +46,6 @@ const distribution = function(config) {
     global.nodeConfig = config;
     this.nodeConfig = config;
   }
-
   return global.distribution;
 };
 
@@ -65,27 +63,20 @@ for (const key in distribution.local) {
   distribution.local.routes.put(distribution.local[key], key);
 }
 
-/* Initialize distribution object */
+// Initialize distribution object
 distribution["all"] = {_isGroup: true};
-distribution["all"].status
-    = require("./distribution/all/status")({gid: "all"});
-distribution["all"].comm
-    = require("./distribution/all/comm")({gid: "all"});
-distribution["all"].gossip
-    = require("./distribution/all/gossip")({gid: "all"});
-distribution["all"].groups
-    = require("./distribution/all/groups")({gid: "all"});
-distribution["all"].routes
-    = require("./distribution/all/routes")({gid: "all"});
-distribution["all"].mem
-    = require("./distribution/all/mem")({gid: "all"});
-distribution["all"].store
-    = require("./distribution/all/store")({gid: "all"});
+distribution["all"].comm = require("./distribution/all/comm")({gid: "all"});
+distribution["all"].gossip = require("./distribution/all/gossip")({gid: "all"});
+distribution["all"].groups = require("./distribution/all/groups")({gid: "all"});
+distribution["all"].mem = require("./distribution/all/mem")({gid: "all"});
+distribution["all"].routes = require("./distribution/all/routes")({gid: "all"});
+distribution["all"].status = require("./distribution/all/status")({gid: "all"});
+distribution["all"].store = require("./distribution/all/store")({gid: "all"});
 
 distribution.node.config = global.nodeConfig;
 module.exports = distribution;
 
-/* The following code is run when distribution.js is run directly */
+// The following code is run when distribution.js is run directly
 if (require.main === module) {
   log(`[node] Starting node with configuration: ${JSON.stringify(global.nodeConfig)}`);
   distribution.node.start(global.nodeConfig.onStart);
