e9ce3646e771fcf9bd7d9820de000f3806b5d5bb
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-02-25 13:12:34 -0500

    add force stop

--- submission/distribution/all/mem.js
+++
@@ -1,5 +0,0 @@
-/* A service that stores key-value pairs in memory across a node group. */
-
-const store = require("./store-service.js");
-
-module.exports = store.createConstructor("mem");
--- submission/distribution/all/store.js
+++
@@ -1,5 +0,0 @@
-/* A service that stores key-value pairs in the local filesystem across a node group. */
-
-const store = require("./store-service.js");
-
-module.exports = store.createConstructor("store");
