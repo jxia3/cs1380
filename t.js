f8b019441a5765bc493d2adba303782ecb13983d
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-03-08 18:47:32 -0500

    add test script

--- t.js
+++ t.js
@@ -7,6 +7,14 @@ const nodes = [
   {ip: "127.0.0.1", port: basePort + 2},
 ];
 
+const dataset = [
+  {"000": "006701199099999 1950 0515070049999999N9 +0000 1+9999"},
+  {"106": "004301199099999 1950 0515120049999999N9 +0022 1+9999"},
+  {"212": "004301199099999 1950 0515180049999999N9 -0011 1+9999"},
+  {"318": "004301265099999 1949 0324120040500001N9 +0111 1+9999"},
+  {"424": "004301265099999 1949 0324180040500001N9 +0078 1+9999"},
+];
+
 distribution.node.start(() => {
   distribution.local.status.spawn(nodes[0], (error, result) => {
     distribution.local.status.spawn(nodes[1], (error, result) => {
@@ -19,10 +27,25 @@ distribution.node.start(() => {
 
 function runTest() {
   distribution.local.groups.put("test", [global.nodeConfig, ...nodes], (error, result) => {
-    console.log("local put result", error, result);
-    distribution.all.groups.put("test", [global.nodeConfig, ...nodes], (error, result) => {
-      console.log("all put result", error, result);
-      console.log();
+    console.log("local put result", error);
+    distribution.test.groups.put("test", [global.nodeConfig, ...nodes], (error, result) => {
+      console.log("test put result", error);
+      let cntr = 0;
+      dataset.forEach((o) => {
+        const key = Object.keys(o)[0];
+        const value = o[key];
+        distribution.test.store.put(value, key, (e, v) => {
+          cntr++;
+          // Once the dataset is in place, run the map reduce
+          if (cntr === dataset.length) {
+            doMapReduce();
+          }
+        });
+      });
     });
   });
+}
+
+function doMapReduce() {
+  console.log("doing map reduce");
 }
