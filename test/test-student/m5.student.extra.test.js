e89edac85b9dda96765b4ffb61c0db36a1910285
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-03-14 12:20:01 -0400

    change

--- test/test-student/m5.student.extra.test.js
+++ test/test-student/m5.student.extra.test.js
@@ -1,8 +1,8 @@
 /*
-    In this file, add your own test case that will confirm your correct implementation of the extra-credit functionality.
-    You are left to decide what the complexity of each test case should be, but trivial test cases that abuse this flexibility might be subject to deductions.
+  In this file, add your own test case that will confirm your correct implementation of the extra-credit functionality.
+  You are left to decide what the complexity of each test case should be, but trivial test cases that abuse this flexibility might be subject to deductions.
 
-    Imporant: Do not modify any of the test headers (i.e., the test('header', ...) part). Doing so will result in grading penalties.
+  Imporant: Do not modify any of the test headers (i.e., the test('header', ...) part). Doing so will result in grading penalties.
 */
 
 const distribution = require("../../config.js");
