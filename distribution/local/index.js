f2415948fd84acd2ab741e0c94a1783dcadb65a6
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-04-01 14:55:28 -0400

    add queue logic

--- distribution/local/index.js
+++ distribution/local/index.js
@@ -3,25 +3,74 @@
 const log = require("../util/log.js");
 const util = require("../util/util.js");
 
+const QUEUE_KEY = "index-queue";
+
 const queueMutex = util.sync.createMutex();
+
+/**
+ * Starts the index loop. This internal function does not accept a callback and
+ * should not be called by external services.
+ */
+function _start() {
+  let active = false;
+  module.exports._interval = setInterval(() => {
+    // Check if another iteration is active
+    if (active) {
+      return;
+    }
+    active = true;
+
+    queueMutex.lock(() => {
+      console.log("checking queue");
+      global.distribution.local.store.get(QUEUE_KEY, (error, queue) => {
+        // Check if there is an item in the queue
+        if (error || queue.length == 0) {
+          queueMutex.unlock(() => {
+            active = false;
+          });
+          return;
+        }
+
+        // Extract the first URL and index the page
+        const url = queue.shift();
+        global.distribution.local.store.put(queue, QUEUE_KEY, (error, result) => {
+          queueMutex.unlock(() => {
+            indexPage(url, (error, result) => {
+              active = false;
+            });
+          });
+        });
+      });
+    });
+  }, 500);
+}
 
 /**
  * Adds a URL to the indexing queue.
  */
-function indexPage(url, callback) {
+function queuePage(url, callback) {
   queueMutex.lock(() => {
     log(`Adding page ${url} to the index queue`);
-    global.distribution.local.store.get("index-queue", (error, queue) => {
+    global.distribution.local.store.get(QUEUE_KEY, (error, queue) => {
       if (error) {
         queue = [url];
       } else {
         queue.push(url);
       }
-      global.distribution.local.store.put(queue, "index-queue", (error, result) => {
+      global.distribution.local.store.put(queue, QUEUE_KEY, (error, result) => {
         queueMutex.unlock(() => callback(error, result));
       });
     });
   });
 }
 
-module.exports = {indexPage};
+/**
+ * Downloads a page with a URL and updates the distributed index.
+ */
+function indexPage(url, callback) {
+  callback = callback === undefined ? (error, result) => {} : callback;
+  console.log("indexing", url);
+  callback(null, null);
+}
+
+module.exports = {queuePage, _start};
--- t.js
+++ t.js
@@ -1,9 +1,12 @@
 const distribution = require("./distribution.js");
 
 distribution.node.start(() => {
-  distribution.local.index.indexPage("https://example.com", () => {
-    distribution.local.index.indexPage("https://b.com", () => {
-      console.log("added pages");
+  distribution.local.index._start();
+  setTimeout(() => {
+    distribution.local.index.queuePage("https://example.com", () => {
+      distribution.local.index.queuePage("https://b.com", () => {
+        console.log("added pages");
+      });
     });
-  });
+  }, 3000);
 });
