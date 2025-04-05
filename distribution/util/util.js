014d562a93937740bfd09013d42c3e08935fbfb9
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-04-05 11:53:09 -0400

    add file

--- distribution/util/util.js
+++ distribution/util/util.js
@@ -2,6 +2,7 @@ const serialization = require("./serialization.js");
 module.exports = {
   serialize: serialization.serialize,
   deserialize: serialization.deserialize,
+  cache: require("./cache.js"),
   compile: require("./compile.js"),
   id: require("./id.js"),
   search: require("./search.js"),
