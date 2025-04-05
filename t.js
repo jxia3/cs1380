19d7635ad2bd3c9d756eda9686d367f4c01a6b22
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-04-05 12:35:05 -0400

    implement LRU

--- t.js
+++ t.js
@@ -1,6 +1,19 @@
 const distribution = require("./distribution.js");
 
-distribution.node.start(() => {
+const cache = distribution.util.cache.createCache(3);
+console.log(cache.getKeys());
+cache.put("a", 1);
+console.log(cache.getKeys());
+cache.put("b", 2);
+console.log(cache.getKeys());
+cache.put("c", 3);
+console.log(cache.getKeys());
+cache.put("d", 4);
+console.log(cache.getKeys());
+console.log(cache.get("c"));
+console.log(cache.getKeys());
+
+/* distribution.node.start(() => {
   distribution.local.groups.put("search", [distribution.node.config], () => {
     console.log("added search group");
     distribution.local.index._start(true, () => {
@@ -9,4 +22,4 @@ distribution.node.start(() => {
       }, 1000);
     });
   });
-});
+});*/
