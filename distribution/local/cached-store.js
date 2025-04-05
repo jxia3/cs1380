74fbe4621f9864dff00e5f600e5904136be7f487
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-04-05 12:53:24 -0400

    add load logic

--- distribution/local/cached-store.js
+++ distribution/local/cached-store.js
@@ -1,6 +1,32 @@
-/* A cached key-value store built on the filesystem store module. */
+/* A cached key-value store built on the filesystem store module. The clear operation
+   is not supported. Keys that are cached cannot be accessed by other store modules. */
 
-function get(config, callback) {}
+const util = require("../util/util.js");
+
+const cache = util.cache.createCache(10000);
+const locks = {};
+
+/**
+ * Retrieves a value from the cache or loads the value into the cache.
+ */
+function get(config, callback) {
+  if (callback === undefined) {
+    return;
+  }
+  const cacheKey = serializeKey(config);
+  if (cache.has(cacheKey)) {
+    callback(null, cache.get(cacheKey));
+    return;
+  }
+
+  loadItem(config, cacheKey, (error, object) => {
+    if (error) {
+      callback(error, null);
+    } else {
+      callback(null, object);
+    }
+  });
+}
 
 function tryGet(config, callback) {}
 
@@ -8,6 +34,61 @@ function put(object, config, callback) {}
 
 function del(config, callback) {}
 
-function clear(groupId, callback) {}
+/**
+ * Loads a key into the cache and possibly evicts the least recently used key.
+ */
+function loadItem(config, cacheKey, callback) {
+  if (callback === undefined) {
+    throw new Error("Load item received no callback");
+  }
+  if (!(cacheKey in locks)) {
+    locks[cacheKey] = util.sync.createMutex();
+  }
 
-module.exports = {get, tryGet, put, del, clear};
+  locks[cacheKey].lock(() => {
+    global.distribution.local.store.get(config, (error, object) => {
+      // Return early on error
+      locks[cacheKey].unlock();
+      if (error) {
+        callback(error, null);
+        return;
+      }
+
+      // Insert the object into the cache
+      const evicted = cache.put(cacheKey, object);
+      if (evicted === null) {
+        callback(null, object);
+        return;
+      }
+
+      // Write back an evicted item
+      locks[evicted].lock(() => {
+        global.distribution.local.store.put(deserializeKey(evicted), (error, result) => {
+          locks[evicted].unlock();
+          if (error) {
+            callback(error, null);
+          } else {
+            callback(null, object);
+          }
+        });
+      });
+    });
+  });
+}
+
+/**
+ * Converts an object configuration into a cache key.
+ */
+function serializeKey(config) {
+  config = util.id.getObjectConfig(config);
+  return JSON.stringify(config);
+}
+
+/**
+ * Converts a cache key into an object configuration.
+ */
+function deserializeKey(cacheKey) {
+  return JSON.parse(cacheKey);
+}
+
+module.exports = {get, tryGet, put, del};
