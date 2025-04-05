af9af76dfc438c705b3452a99313f5b38f91cc31
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-04-05 13:16:14 -0400

    add put

--- distribution/local/cached-store.js
+++ distribution/local/cached-store.js
@@ -52,7 +52,25 @@ function tryGet(config, callback) {
   });
 }
 
-function put(object, config, callback) {}
+/**
+ * Modifies a cached value that is written back when evicted.
+ */
+function put(object, config, callback) {
+  callback = callback === undefined ? (error, result) => {} : callback;
+  const cacheKey = serializeKey(config);
+  if (cache.has(cacheKey)) {
+    cacheItem(cacheKey, object, callback);
+    return;
+  }
+
+  loadItem(config, cacheKey, (error, exists, object) => {
+    if (error) {
+      callback(error, null, null);
+    } else {
+      cacheItem(cacheKey, object, callback);
+    }
+  });
+}
 
 function del(config, callback) {}
 
@@ -79,25 +97,46 @@ function loadItem(config, cacheKey, callback) {
         return;
       }
 
-      // Insert the object into the cache
-      const evicted = cache.put(cacheKey, object);
-      if (evicted === null) {
-        callback(null, true, object);
-        return;
-      }
-
-      // Write back an evicted item
-      locks[evicted.key].lock(() => {
-        const storeConfig = deserializeKey(evicted.key);
-        global.distribution.local.store.put(evicted.value, storeConfig, (error, result) => {
-          locks[evicted.key].unlock();
-          if (error) {
-            callback(error, null, null);
-          } else {
-            callback(null, true, object);
-          }
-        });
+      // Add key-value pair to cache
+      cacheItem(cacheKey, object, (error, result) => {
+        if (error) {
+          callback(error, null, null);
+        } else {
+          callback(null, true, object);
+        }
       });
+    });
+  });
+}
+
+/**
+ * Inserts a key-value pair into the cache and handles evictions.
+ */
+function cacheItem(cacheKey, object, callback) {
+  if (callback === undefined) {
+    throw new Error("Cache item received no callback");
+  }
+  if (!(cacheKey in locks)) {
+    locks[cacheKey] = util.sync.createMutex();
+  }
+
+  // Insert the object into the cache
+  const evicted = cache.put(cacheKey, object);
+  if (evicted === null) {
+    callback(null, object);
+    return;
+  }
+
+  // Write back an evicted item
+  locks[evicted.key].lock(() => {
+    const storeConfig = deserializeKey(evicted.key);
+    global.distribution.local.store.put(evicted.value, storeConfig, (error, result) => {
+      locks[evicted.key].unlock();
+      if (error) {
+        callback(error, null);
+      } else {
+        callback(null, object);
+      }
     });
   });
 }
