a1c97089a51bc00132093d73ad2655dc4d07e9da
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-04-05 21:34:10 -0400

    reintroduce locks

--- distribution/local/cached-store.js
+++ distribution/local/cached-store.js
@@ -7,7 +7,8 @@ const util = require("../util/util.js");
 
 const NOT_FOUND_MARK = params.notFoundMark;
 
-const cache = util.cache.createCache(1000);
+const cache = util.cache.createCache(100);
+const locks = {};
 
 /**
  * Retrieves a value from the cache or loads the value into the cache.
@@ -16,17 +17,17 @@ function get(config, callback) {
   if (callback === undefined) {
     return;
   }
-  const cacheKey = serializeKey(config);
-  if (cacheKey instanceof Error) {
-    callback(cacheKey, null);
+  const keys = serializeKey(config);
+  if (keys instanceof Error) {
+    callback(keys, null);
     return;
   }
 
-  if (cache.has(cacheKey)) {
-    callback(null, cache.get(cacheKey));
+  if (cache.has(keys.cacheKey)) {
+    callback(null, cache.get(keys.cacheKey).data);
     return;
   }
-  loadItem(config, cacheKey, (error, exists, object) => {
+  loadItem(config, keys, (error, exists, object) => {
     if (error) {
       callback(error, null);
     } else if (!exists) {
@@ -44,17 +45,17 @@ function tryGet(config, callback) {
   if (callback === undefined) {
     return;
   }
-  const cacheKey = serializeKey(config);
-  if (cacheKey instanceof Error) {
-    callback(cacheKey, null);
+  const keys = serializeKey(config);
+  if (keys instanceof Error) {
+    callback(keys, null);
     return;
   }
 
-  if (cache.has(cacheKey)) {
-    callback(null, true, cache.get(cacheKey));
+  if (cache.has(keys.cacheKey)) {
+    callback(null, true, cache.get(keys.cacheKey).data);
     return;
   }
-  loadItem(config, cacheKey, (error, exists, object) => {
+  loadItem(config, keys, (error, exists, object) => {
     if (error) {
       callback(error, null, null);
     } else {
@@ -68,12 +69,12 @@ function tryGet(config, callback) {
  */
 function put(object, config, callback) {
   callback = callback === undefined ? (error, result) => {} : callback;
-  const cacheKey = serializeKey(config);
-  if (cacheKey instanceof Error) {
-    callback(cacheKey, null);
+  const keys = serializeKey(config);
+  if (keys instanceof Error) {
+    callback(keys, null);
     return;
   }
-  cacheItem(cacheKey, object, callback);
+  cacheItem(keys, object, true, callback);
 }
 
 /**
@@ -81,57 +82,75 @@ function put(object, config, callback) {
  */
 function del(config, callback) {
   callback = callback === undefined ? (error, result) => {} : callback;
-  const cacheKey = serializeKey(config);
-  if (cacheKey instanceof Error) {
-    callback(cacheKey, null);
+  const keys = serializeKey(config);
+  if (keys instanceof Error) {
+    callback(keys, null);
     return;
   }
 
-  store.del(config, (error, object) => {
-    let removed = null;
-    if (cache.has(cacheKey)) {
-      removed = cache.del(cacheKey);
-    }
+  // Create lock for sync key
+  if (!(keys.syncKey in locks)) {
+    locks[keys.syncKey] = util.sync.createMutex();
+  }
+  const lock = locks[keys.syncKey];
 
-    if (error) {
-      if (error[NOT_FOUND_MARK] && removed !== null) {
-        callback(null, removed.value);
-      } else {
-        callback(error, null);
+  lock.lock(() => {
+    store.del(config, (error, object) => {
+      // Remove key from cache
+      let removed = null;
+      if (cache.has(keys.cacheKey)) {
+        removed = cache.del(keys.cacheKey);
       }
-    } else if (removed !== null) {
-      callback(null, removed.value);
-    } else {
-      callback(null, object);
-    }
+      lock.unlock();
+
+      // Check error conditions
+      if (error) {
+        if (error[NOT_FOUND_MARK] && removed !== null) {
+          callback(null, removed.value.data);
+        } else {
+          callback(error, null);
+        }
+      } else if (removed !== null) {
+        callback(null, removed.value.data);
+      } else {
+        callback(null, object);
+      }
+    });
   });
 }
 
 /**
  * Loads a key into the cache and possibly evicts the least recently used key.
  */
-function loadItem(config, cacheKey, callback) {
+function loadItem(config, keys, callback) {
   if (callback === undefined) {
     throw new Error("Load item received no callback");
   }
+  if (!(keys.syncKey in locks)) {
+    locks[keys.syncKey] = util.sync.createMutex();
+  }
+  const lock = locks[keys.syncKey];
 
-  store.tryGet(config, (error, exists, object) => {
-    // Check error conditions
-    if (error) {
-      callback(error, null, null);
-      return;
-    } else if (!exists) {
-      callback(null, false, null);
-      return;
-    }
-
-    // Add key-value pair to cache
-    cacheItem(cacheKey, object, (error, result) => {
+  lock.lock(() => {
+    store.tryGet(config, (error, exists, object) => {
+      // Check error conditions
+      lock.unlock();
       if (error) {
         callback(error, null, null);
-      } else {
-        callback(null, true, object);
+        return;
+      } else if (!exists) {
+        callback(null, false, null);
+        return;
       }
+
+      // Add key-value pair to cache
+      cacheItem(keys, object, false, (error, result) => {
+        if (error) {
+          callback(error, null, null);
+        } else {
+          callback(null, true, object);
+        }
+      });
     });
   });
 }
@@ -139,21 +158,44 @@ function loadItem(config, cacheKey, callback) {
 /**
  * Inserts a key-value pair into the cache and handles evictions.
  */
-function cacheItem(cacheKey, object, callback) {
+function cacheItem(keys, object, dirty, callback) {
   if (callback === undefined) {
     throw new Error("Cache item received no callback");
   }
 
   // Insert the object into the cache
-  const evicted = cache.put(cacheKey, object);
-  if (evicted === null) {
+  const evicted = cache.put(keys.cacheKey, {data: object, dirty});
+  if (evicted === null || !evicted.value.dirty) {
     callback(null, object);
     return;
   }
 
   // Write back an evicted item
   const storeConfig = deserializeKey(evicted.key);
-  store.put(evicted.value, storeConfig, callback);
+  const syncKey = store._getSyncKey(storeConfig);
+  if (!(syncKey in locks)) {
+    throw new Error(`Key '${syncKey}' does not have a lock`);
+  }
+  const lock = locks[syncKey];
+
+  lock.lock(() => {
+    // Check if the value has been reinserted
+    let value = evicted.value.data;
+    if (cache.has(evicted.key)) {
+      const entry = cache.get(evicted.key);
+      if (entry.dirty) {
+        value = entry.data;
+      } else {
+        cache.put(evicted, {data: value, dirty: false});
+      }
+    }
+
+    // Write the most updated value to storage
+    store.put(value, storeConfig, (error, result) => {
+      lock.unlock();
+      callback(error, result);
+    });
+  });
 }
 
 /**
@@ -161,19 +203,31 @@ function cacheItem(cacheKey, object, callback) {
  */
 function flush(config, callback) {
   callback = callback === undefined ? (error, result) => {} : callback;
-  const cacheKey = serializeKey(config);
-  if (cacheKey instanceof Error) {
-    callback(cacheKey, null);
+  const keys = serializeKey(config);
+  if (keys instanceof Error) {
+    callback(keys, null);
     return;
   }
 
-  if (!cache.has(cacheKey)) {
-    callback(null, null);
-    return;
+  // Get lock for sync key
+  if (!(keys.syncKey in locks)) {
+    throw new Error(`Key '${keys.syncKey} has no lock'`);
   }
-  const storeConfig = deserializeKey(cacheKey);
-  const object = cache.get(cacheKey);
-  store.put(object, storeConfig, callback);
+  const lock = locks[keys.syncKey];
+
+  // Write the item if it still exists
+  lock.lock(() => {
+    if (!cache.has(keys.cacheKey)) {
+      lock.unlock();
+      callback(null, null);
+    } else {
+      const value = cache.get(keys.cacheKey).data;
+      store.put(value, config, (error, result) => {
+        lock.unlock();
+        callback(error, result);
+      });
+    }
+  });
 }
 
 /**
@@ -184,7 +238,10 @@ function serializeKey(config) {
   if (config.key === null) {
     return new Error("Null keys are not supported");
   }
-  return JSON.stringify(config);
+  return {
+    cacheKey: JSON.stringify(config),
+    syncKey: store._getSyncKey(config),
+  };
 }
 
 /**
