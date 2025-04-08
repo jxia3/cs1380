dca1d9d25077e4295d3000ada26ef76a8b16e33c
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-04-08 16:08:44 -0400

    add

--- distribution/all/query2.js
+++ distribution/all/query2.js
@@ -6,10 +6,41 @@ const util = require("../util/util.js");
 
 const GROUP = params.searchGroup;
 
+const cache = util.cache.createCache(20000);
+
+/**
+ * Routes remote requests to get the data associated with each term and caches the data.
+ */
 function lookupTerms(terms, callback) {
+  checkContext(this.gid, this.hash);
   if (callback === undefined) {
     return;
   }
+
+  global.distribution.local.groups.get(this.gid, (error, group) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    const results = {};
+
+    // Compute request batches
+    const batches = {};
+    for (const term of terms) {
+      if (cache.has(term.text)) {
+        results[term.text] = cache.get(term.text);
+        continue;
+      }
+      const termKey = util.search.createFullTermKey(term.text);
+      const node = util.id.applyHash(termKey, group, this.hash);
+      if (!(node in batches)) {
+        batches[node] = [];
+      }
+      batches[node].push({term: term.text, key: termKey});
+    }
+
+    console.log(batches);
+  });
 }
 
 /**
--- t.js
+++ t.js
@@ -1,6 +1,29 @@
 const distribution = require("./distribution.js");
 
-setTimeout(() => {
-  const terms = distribution.util.search.calcTerms("google ai model");
-  console.log(terms);
-}, 1000);
+const GROUP = distribution.searchParams.searchGroup;
+const RESET = true;
+
+const localNode = {
+  ip: distribution.node.config.ip,
+  port: distribution.node.config.port,
+};
+const nodes = [localNode];
+const urls = ["https://deepmind.google"];
+const query = "google ai model";
+
+distribution.node.start(() => {
+  distribution.local.groups.put(GROUP, nodes, (error, result) => {
+    if (error) {
+      throw error;
+    }
+    distribution[GROUP].groups.put(GROUP, nodes, (error, result) => {
+      if (Object.keys(error).length > 0) {
+        throw error;
+      }
+      setTimeout(() => {
+        const terms = distribution.util.search.calcTerms("google ai model").terms;
+        distribution[GROUP].query2.lookupTerms(terms, console.log);
+      }, 100);
+    });
+  });
+});
