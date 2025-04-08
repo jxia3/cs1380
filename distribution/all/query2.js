7033ed1cb3e3069131d7da697f941a40061e3324
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-04-08 15:55:11 -0400

    remove stopword

---
+++ distribution/all/query2.js
@@ -0,0 +1,7 @@
+/* A service that supports low-latency term lookups. */
+
+function lookupTerms(terms, callback) {
+    if (callback === undefined) {
+        return;
+    }
+}
--- t.js
+++ t.js
@@ -1,15 +1,6 @@
 const distribution = require("./distribution.js");
 
-const RESET = true;
-
-distribution.node.start(() => {
-  distribution.local.groups.put("search", [distribution.node.config], () => {
-    distribution.local.crawl._start(RESET, () => {
-      distribution.local.index._start(RESET, () => {
-        setTimeout(() => {
-          distribution.local.crawl.queueURLs(["https://www.nba.com"], () => {});
-        }, 1000);
-      });
-    });
-  });
-});
+setTimeout(() => {
+  const terms = distribution.util.search.calcTerms("google ai model")
+  console.log(terms)
+}, 1000)
