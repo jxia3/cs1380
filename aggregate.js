6329b7f30761d2bec0af177b87d4a6141d02ecec
jxia3 <97870693+jxia3@users.noreply.github.com>
2025-04-10 17:31:44 -0400

    add job start

--- aggregate.js
+++ aggregate.js
@@ -4,7 +4,22 @@ const distribution = run.distribution;
 const GROUP = run.GROUP;
 
 run.startLocal(() => {
-  distribution[GROUP].mr.exec({
-
+  distribution[GROUP].store.get(null, (error, keys) => {
+    if (Object.keys(error).length > 0) {
+      throw error;
+    }
+    console.log("Got keys:", keys.length);
+    const operation = {
+      map: (terms) => {
+        console.log(terms);
+        return terms;
+      },
+      reduce: (key, values) => {
+        console.log(key, values);
+      },
+    };
+    distribution[GROUP].mr.exec(operation, (error, result) => {
+      console.log(error, result);
+    });
   });
 });
